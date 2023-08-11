460f4aae741ef89269ea7ee1f4903fb11145b754
Gowtham-egov <112694085+Gowtham-egov@users.noreply.github.com>
2023-08-11 15:43:04 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -1,8 +1,12 @@
 library 'ci-libs'
-
 def call(Map pipelineParams) {
     echo "Environment: ${pipelineParams.environment}"
-podTemplate(yaml: """
+    // Print out the variables
+    echo "pipelineParams.helmDir: ${pipelineParams.helmDir}"
+    echo "env.CLUSTER_CONFIGS: ${env.CLUSTER_CONFIGS}"
+    echo "pipelineParams.environment: ${pipelineParams.environment}"
+    echo "env.IMAGES: ${env.IMAGES}"
+    podTemplate(yaml: """
 kind: Pod
 metadata:
   name: egov-deployer
@@ -13,34 +17,32 @@ spec:
     command:
     - cat
     tty: true
-    env:  
+    env:
     volumeMounts:
       - name: kube-config
-        mountPath: /root/.kube     
+        mountPath: /root/.kube
     resources:
       requests:
         memory: "256Mi"
         cpu: "200m"
       limits:
         memory: "256Mi"
-        cpu: "200m"  
-  volumes: 
+        cpu: "200m"
+  volumes:
   - name: kube-config
     secret:
-        secretName: "${pipelineParams.environment}-kube-config"                    
+        secretName: "${pipelineParams.environment}-kube-config"
 """
     ) {
         node(POD_LABEL) {
             git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
                 stage('Deploy Images') {
-                        container(name: 'egov-deployer', shell: '/bin/sh') {
-                            sh """
-                                /opt/egov/egov-deployer deploy --helm-dir `pwd`/${pipelineParams.helmDir} -c=${env.CLUSTER_CONFIGS}  -e ${pipelineParams.environment} "${env.IMAGES}"
-                            """
-                            }
+                    container(name: 'egov-deployer', shell: '/bin/sh') {
+                        sh """
+                            /opt/egov/egov-deployer deploy --helm-dir `pwd`/${pipelineParams.helmDir} -c=${env.CLUSTER_CONFIGS}  -e ${pipelineParams.environment} "${env.IMAGES}"
+                        """
+                    }
                 }
         }
     }
-
-
 }
