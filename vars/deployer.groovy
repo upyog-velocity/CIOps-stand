374f7e191cd8fb5a46ceabc35775673f95a31b7b
nikhilmulinti-egov <127198713+nikhilmulinti-egov@users.noreply.github.com>
2023-08-11 17:18:06 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -35,7 +35,7 @@ spec:
         secretName: "${pipelineParams.environment}-kube-config"
 """
     ) {
-        node {
+        node(POD_LABEL) {
             git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
                 stage('Deploy Images') {
                     container(name: 'egov-deployer', shell: '/bin/sh') {
