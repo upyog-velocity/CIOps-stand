a57898f537fb37817f6e937354de60b0c0cb7bf7
nikhilmulinti-egov <127198713+nikhilmulinti-egov@users.noreply.github.com>
2023-08-14 14:42:41 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -19,9 +19,9 @@ spec:
     - cat
     tty: true
     env:
-    volumeMounts:
-      - name: kube-config
-        mountPath: /root/.kube
+    // volumeMounts:
+      // - name: kube-config
+        // mountPath: /root/.kube
     resources:
       requests:
         memory: "256Mi"
