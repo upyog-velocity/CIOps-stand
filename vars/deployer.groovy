bf3e099ec0c9207e043b693785a1b08a270309fe
nikhilmulinti-egov <127198713+nikhilmulinti-egov@users.noreply.github.com>
2023-08-14 14:31:20 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -36,7 +36,6 @@ spec:
 """
     ) {
         node(POD_LABEL) {
-            git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
                 stage('Deploy Images') {
                     container(name: 'egov-deployer', shell: '/bin/sh') {
                         sh """
